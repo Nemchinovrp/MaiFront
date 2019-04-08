d3bc5fdcc1184ddafdcd95159e838baab0edf0da
Roman Nemchinov <nemchinovrp@yandex.ru>
2019-04-08 14:00:32 +0300

    minor fix

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,16 +1,12 @@
 import {Component, OnInit} from '@angular/core';
 import {StorageService} from '../service/storage.service';
 import {CurrencyService} from '../service/currency.service';
-import {Currency} from '../model/currency';
 import {UserService} from '../service/user.service';
 import {VacancyService} from '../service/vacancy.service';
 import {ResumeService} from '../service/resume.service';
-import {User} from '../model/user';
 import 'rxjs/add/observable/interval';
 import 'rxjs/add/operator/startWith';
 import 'rxjs/add/operator/switchMap';
-import {Bet} from '../model/bet';
-import {Vacancy} from '../model/Vacancy';
 import {Resume} from '../model/Resume';
 import {Dynamic} from '../model/Dynamic';
 
@@ -20,15 +16,9 @@ import {Dynamic} from '../model/Dynamic';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
-  private nameUser: string;
-  private currencies: Currency[];
-  private users: User[];
-  private currency: Currency;
-  private bet: Bet;
-  private vacancys: Vacancy[];
   private resumes: Resume[];
   private dynamics: Dynamic[];
-  private alive: boolean;
+
 
   constructor(private storage: StorageService, private currencyServiсe: CurrencyService,
               private userService: UserService, private vacancyService: VacancyService, private resumeService: ResumeService) {
@@ -50,6 +40,7 @@ export class MainComponent implements OnInit {
       }
     );
   }
+
   getAllResume() {
     this.resumeService.getResume().subscribe(
       res => {
